d597c71c15a157b2595ed824c8c0caf055b9d883
C.Stein <C.Stein@WS064>
2021-11-12 07:58:46 +0100

    drs,ads,bms,str,dms added

--- serialtesting/Program.cs
+++ serialtesting/Program.cs
@@ -1,30 +1,14 @@
 ﻿using System;
-//using System.IO.Ports;
 using GodSharp.SerialPort;
 using GodSharp.SerialPort.Extensions;
 using CommandLine;
 using coptions;
 using OptionAttribute = coptions.OptionAttribute;
+using System.Text;
 
-
-[ApplicationInfo(Help = "Example: programname.exe -d com8 -c 1 -v 1")]
+[ApplicationInfo(Help = "Example: programname.exe -d com8 -c DRS -a 1 -m 3 -v 1")]
 public class Options
 {
-    //	[Flag('s', "silent", Help = "Produce no output.")]
-    //	public bool Silent;
-
-    //	[Option('n', "name", "NAME", Help = "Name of user.")]
-    //	public string Name
-    //	{
-    //		get { return _name; }
-    //		set
-    //		{
-    //			if (String.IsNullOrWhiteSpace(value))
-    //				throw new InvalidOptionValueException("Name must not be blank");
-    //			_name = value;
-    //		}
-    //	}
-    //	private string _name;
 
     [Option('d', "comport", "COMPORT", Help = "Comport Windows \"COM1\" or Linux \"/dev/ttySx\" ")]
     public string Comport
@@ -39,7 +23,7 @@ public class Options
     }
     private string _comport;
 
-    [Option('c', "commands", "CMD", Help = "STR | DMS | ERR ... ")]
+    [Option('c', "command", "CMD", Help = "Command STR/DRS/DMS/BMS ... ")]
     public string cmd
     {
         get { return _cmd; }
@@ -79,15 +63,15 @@ public class Options
     private string _moduleno;
 
     [Option('v', "value", "VALUE", Help = "Value")]
-    public string Svalue
+    public string value
     {
-        get { return _svalue; }
+        get { return _value; }
         set
         {
-            _svalue = value;
+            _value = value;
         }
     }
-    private string _svalue;
+    private string _value;
 }
 
 namespace GodSharp.NetCore.ConsoleSample
@@ -97,108 +81,118 @@ namespace GodSharp.NetCore.ConsoleSample
     {
         static int Main(string[] args)
         {
-            try {
-                   HOYA.hoya hoya = new HOYA.hoya();
+            try
+            {
+                HOYA.hoya hoya = new HOYA.hoya();
 
-                    Options opt = CliParser.Parse<Options>(args);
+                Options opt = CliParser.Parse<Options>(args);
 
-//            string test = hoya.RequestData("1", "4", "STR");
-                    GodSerialPort gsp = new GodSerialPort(opt.Comport, 38400, 0, 8, 1, 0);
+                //                GodSerialPort gsp = new GodSerialPort(opt.Comport, 115200, 0, 8, 1, 0);
+                GodSerialPort gsp = new GodSerialPort(opt.Comport, 38400, 0, 8, 1, 0);
 
-                    gsp.UseDataReceived(true, (sp, bytes) =>
-                        {
-                            if (bytes != null && bytes.Length > 0)
-                            {
-                                string buffer = string.Join(" ", bytes);
-                                Console.WriteLine("receive data:" + buffer);
+                gsp.UseDataReceived(true, (sp, bytes) =>
+                {
+                    if (bytes != null && bytes.Length > 0)
+                    {
+                        string buffer = string.Join(" ", bytes);
+                        Console.WriteLine("receive data:" + buffer);
 
-            //                    string test = HOYA.hoya.ByteArrayToString(bytes);
-            //                    Console.WriteLine("receive test:" + test);
-            /*                    if (opt.bDebug)
-                                {
-                                    string hexString = SCP.hoya.ByteArrayToString(bytes);
-                                    Console.WriteLine("Debug response: "+hexString);
-                                }
-                                if (opt.Mfunction == "1")
-                                {
-                                    byte[] werte = new byte[2]; 
-                                    scp.ReadDPAna(bytes, werte);
-                                    string hexString = SCP.hoya.ByteArrayToString(werte);
-                                    //                    Console.WriteLine("Data: " + hexString);
-                                    int num = Int32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
-                                    Console.WriteLine(num.ToString()); // Wert 
-                                }
-                                if (opt.Mfunction == "3")
-                                {
-                                    byte[] werte = new byte[1]; 
-                                    scp.ReadDPDig(bytes, werte);
-                                    string hexString = SCP.hoya.ByteArrayToString(werte);
-                                    //                    Console.WriteLine("Data: " + hexString);
-                                    int num = Int32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
-                                    Console.WriteLine(num.ToString()); // Wert 
-                                }*/
-                            }
-                        });
+                        string test = ByteArrayToString(bytes);
+                        Console.WriteLine("receive test:" + test);
+                    //---------------
 
-                    bool flag = gsp.Open();
+                }
+                });
 
-                   if (!flag)
-                   {
-                        //Exit();
-                   }
+                bool flag = gsp.Open();
 
-            //SerialValueTest();
-                   if (opt.cmd == "STR")
-                        {
-                                //  send read request
-                                string dd = hoya.RequestData(opt.address, opt.moduleno, opt.cmd); 
-                                gsp.WriteHexString(dd);
+                if (!flag)
+                {
+                    Exit();
+                }
 
-                                System.Threading.Thread.Sleep(150);
-                        }
-                   if (opt.cmd == "DMS")
-                        {
-                            //  send read request
-                            string dd = hoya.RequestData(opt.address, opt.moduleno, opt.cmd);
-                            gsp.WriteHexString(dd);
+                if (opt.cmd == "STR")
+                {
+                    string dd = hoya.RequestSTR(opt.address, opt.moduleno, opt.value);
+                    gsp.WriteHexString(dd);
+                    System.Threading.Thread.Sleep(150);
+                }
+                if (opt.cmd == "DMS")
+                {
+                    string dd = hoya.RequestDMS(opt.address, opt.moduleno, opt.value);
+                    gsp.WriteHexString(dd);
+                    System.Threading.Thread.Sleep(150);
+                }
+                if (opt.cmd == "DRS")
+                {
+                    string dd = hoya.RequestDRS(opt.address, opt.moduleno, opt.value);
+                    gsp.WriteHexString(dd);
+                    System.Threading.Thread.Sleep(150);
+                }
+                if (opt.cmd == "ADS")
+                {
+                    string dd = hoya.RequestADS(opt.address, opt.moduleno, opt.value);
+                    gsp.WriteHexString(dd);
+                    System.Threading.Thread.Sleep(150);
+                }
+                if (opt.cmd == "BMS")
+                {
+                    string dd = hoya.RequestBMS(opt.address, opt.moduleno, opt.value);
+                    gsp.WriteHexString(dd);
+                    System.Threading.Thread.Sleep(150);
+                }
 
-                            System.Threading.Thread.Sleep(150);
-                        }
-
+                //------------------
                 gsp.Close();
+
+                static void Exit()
+                {
+                    Console.WriteLine("press any key to quit.");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
                 return 0;
             }
-                        catch (CliParserExit)
-                        {
-                            // --help
-                            return 0;
-
-                        }
-                        catch (Exception e)
-                        {
-                            // unknown options etc...
-                            Console.Error.WriteLine("Fatal Error: " + e.Message);
-                            return 1;
-                        }
-
-            string ToHexString(float f)
+            catch (CliParserExit)
             {
-                var bytes = BitConverter.GetBytes(f);
-                var i = BitConverter.ToInt32(bytes, 0);
-                return "0x" + i.ToString("X8");
+                // --help
+                return 0;
+
             }
-
-            float FromHexString(string s)
+            catch (Exception e)
             {
-                var i = Convert.ToInt32(s, 16);
-                var bytes = BitConverter.GetBytes(i);
-                return BitConverter.ToSingle(bytes, 0);
+                // unknown options etc...
+                Console.Error.WriteLine("Fatal Error: " + e.Message);
+                return 1;
+            }
+        }
+            string ToHexString(float f)
+                {
+                    var bytes = BitConverter.GetBytes(f);
+                    var i = BitConverter.ToInt32(bytes, 0);
+                    return "0x" + i.ToString("X8");
+                }
+
+                float FromHexString(string s)
+                {
+                    var i = Convert.ToInt32(s, 16);
+                    var bytes = BitConverter.GetBytes(i);
+                    return BitConverter.ToSingle(bytes, 0);
+                }
+            public static byte[] StringToByteArray(String hex)
+            {
+                int NumberChars = hex.Length;
+                byte[] bytes = new byte[NumberChars / 2];
+                for (int i = 0; i < NumberChars; i += 2)
+                    bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+                return bytes;
+            }
+            public static string ByteArrayToString(byte[] ba)
+            {
+                StringBuilder hex = new StringBuilder(ba.Length * 2);
+                foreach (byte b in ba)
+                    hex.AppendFormat("{0:x2}", b);
+                return hex.ToString();
             }
         }
     }
-}
-                    // IEEE754 it works
-                    //               var hexString = ToHexString(-10.5F);
-                    //               var f = FromHexString(hexString);
-                    //               Console.WriteLine(hexString.ToString());
-                    //               Console.WriteLine(f.ToString());
--- serialtesting/hoya.cs
+++ serialtesting/hoya.cs
@@ -8,6 +8,365 @@ namespace HOYA
     {
 
         //-------------------------------------------------------------------------------------
+        public string RequestSTR(string address, string modulenumber, string value)
+        {
+        
+            string f2 = value;
+            byte[] dat = new byte[6];
+            byte[] adr = Encoding.ASCII.GetBytes(address);
+            byte[] mod = Encoding.ASCII.GetBytes(modulenumber);
+
+            dat[0] = adr[0];// 0x31;// (byte)address;                             // address
+            dat[1] = mod[0];// 0x34;// (byte)modulenumber;                        // module number
+            dat[2] = 0x53;                                  // cmd byte 1
+            dat[3] = 0x54;                                  // cmd byte 2
+            dat[4] = 0x52;                                  // cmd byte 3
+            dat[5] = 0x03;                                      // ETX
+
+            int sum = 0;
+            foreach (int number in dat)
+            {
+                sum += number;
+            }
+//            Console.WriteLine(sum);
+            string hilf = sum.ToString("X");
+            string crc1 = hilf.Substring(1, 1);
+            string crc2 = hilf.Substring(2, 1);
+//            Console.WriteLine(hilf);
+
+            byte[] data = new byte[11];
+            byte[] array = Encoding.ASCII.GetBytes(crc1);
+            byte[] array1 = Encoding.ASCII.GetBytes(crc2);
+
+            data[0] = 0x02;
+            data[1] = dat[0];
+            data[2] = dat[1];
+            data[3] = dat[2];
+            data[4] = dat[3];
+            data[5] = dat[4];
+            data[6] = dat[5];
+
+            data[7] = array[0];
+            data[8] = array1[0];
+            data[9] = 0x0D;                                      // Carriage Return
+            data[10] = 0x0A;                                     // Line Feed
+
+
+            string tes = ByteArrayToString(data);
+            return tes;
+        }
+        //-------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------
+        public string RequestADR(string address, string modulenumber, string value)
+        {
+            string f1 = address;
+            string f3 = value;
+            byte[] dat = new byte[6];
+            byte[] adr = Encoding.ASCII.GetBytes(address);
+            byte[] mod = Encoding.ASCII.GetBytes(modulenumber);
+
+            dat[0] = 0x40; // @
+            dat[1] = mod[0];// 0x34;// (byte)modulenumber;                        // module number
+            dat[2] = 0x41;                                  // cmd byte 1
+            dat[3] = 0x44;                                  // cmd byte 2
+            dat[4] = 0x52;                                  // cmd byte 3
+            dat[5] = 0x03;                                      // ETX
+
+            int sum = 0;
+            foreach (int number in dat)
+            {
+                sum += number;
+            }
+//            Console.WriteLine(sum);
+            string hilf = sum.ToString("X");
+            string crc1 = hilf.Substring(1, 1);
+            string crc2 = hilf.Substring(2, 1);
+//            Console.WriteLine(hilf);
+
+            byte[] data = new byte[11];
+            byte[] array = Encoding.ASCII.GetBytes(crc1);
+            byte[] array1 = Encoding.ASCII.GetBytes(crc2);
+
+            data[0] = 0x02;
+            data[1] = dat[0];
+            data[2] = dat[1];
+            data[3] = dat[2];
+            data[4] = dat[3];
+            data[5] = dat[4];
+            data[6] = dat[5];
+
+            data[7] = array[0];
+            data[8] = array1[0];
+            data[9] = 0x0D;                                      // Carriage Return
+            data[10] = 0x0A;                                     // Line Feed
+
+
+            string tes = ByteArrayToString(data);
+            return tes;
+        }
+        //-------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------
+        public string RequestDMS(string address, string modulenumber, string value)
+        {
+          
+            byte[] dat = new byte[9];
+            byte[] adr = Encoding.ASCII.GetBytes(address);
+            byte[] mod = Encoding.ASCII.GetBytes(modulenumber);
+
+            dat[0] = adr[0];                                // address
+            dat[2] = 0x53;                                  // cmd byte 1
+            dat[3] = 0x54;                                  // cmd byte 2
+            dat[4] = 0x52;                                  // cmd byte 3
+            if(value == "0")
+            {// ausschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x30;
+            }
+            if(value == "1")
+            { // einschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x31;
+            }
+            dat[9] = 0x03;                                      // ETX
+
+            int sum = 0;
+            foreach (int number in dat)
+            {
+                sum += number;
+            }
+//            Console.WriteLine(sum);
+            string hilf = sum.ToString("X");
+            string crc1 = hilf.Substring(1, 1);
+            string crc2 = hilf.Substring(2, 1);
+//            Console.WriteLine(hilf);
+
+            byte[] data = new byte[15];
+            byte[] array = Encoding.ASCII.GetBytes(crc1);
+            byte[] array1 = Encoding.ASCII.GetBytes(crc2);
+
+            data[0] = 0x02;
+            data[1] = dat[0];
+            data[2] = dat[1];
+            data[3] = dat[2];
+            data[4] = dat[3];
+            data[5] = dat[4];
+            data[6] = dat[5];
+
+            data[7] = dat[7];
+            data[8] = dat[8];
+            data[9] = dat[9];
+            data[10] = dat[10];
+
+            data[11] = array[0];
+            data[12] = array1[0];
+            data[13] = 0x0D;                                      // Carriage Return
+            data[14] = 0x0A;                                     // Line Feed
+
+
+            string tes = ByteArrayToString(data);
+            return tes;
+        }
+
+        //-------------------------------------------------------------------------------------
+        public string RequestDRS(string address, string modulenumber, string value)
+        {
+            byte[] dat = new byte[9];
+            byte[] adr = Encoding.ASCII.GetBytes(address);
+            byte[] mod = Encoding.ASCII.GetBytes(modulenumber);
+
+            dat[0] = adr[0];                                // address
+            dat[2] = 0x44;                                  // cmd byte 1
+            dat[3] = 0x52;                                  // cmd byte 2
+            dat[4] = 0x53;                                  // cmd byte 3
+            if(value == "0")
+            {// ausschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x30;
+            }
+            if(value == "1")
+            { // einschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x31;
+            }
+            dat[9] = 0x03;                                      // ETX
+
+            int sum = 0;
+            foreach (int number in dat)
+            {
+                sum += number;
+            }
+//            Console.WriteLine(sum);
+            string hilf = sum.ToString("X");
+            string crc1 = hilf.Substring(1, 1);
+            string crc2 = hilf.Substring(2, 1);
+//            Console.WriteLine(hilf);
+
+            byte[] data = new byte[15];
+            byte[] array = Encoding.ASCII.GetBytes(crc1);
+            byte[] array1 = Encoding.ASCII.GetBytes(crc2);
+
+            data[0] = 0x02;
+            data[1] = dat[0];
+            data[2] = dat[1];
+            data[3] = dat[2];
+            data[4] = dat[3];
+            data[5] = dat[4];
+            data[6] = dat[5];
+
+            data[7] = dat[7];
+            data[8] = dat[8];
+            data[9] = dat[9];
+            data[10] = dat[10];
+
+            data[11] = array[0];
+            data[12] = array1[0];
+            data[13] = 0x0D;                                      // Carriage Return
+            data[14] = 0x0A;                                     // Line Feed
+
+
+            string tes = ByteArrayToString(data);
+            return tes;
+        }
+        //-------------------------------------------------------------------------------------
+       //-------------------------------------------------------------------------------------
+        public string RequestBMS(string address, string modulenumber, string value)
+        {
+            byte[] dat = new byte[9];
+            byte[] adr = Encoding.ASCII.GetBytes(address);
+            byte[] mod = Encoding.ASCII.GetBytes(modulenumber);
+            
+            dat[0] = adr[0];                                // address
+            dat[2] = 0x42;                                  // cmd byte 1
+            dat[3] = 0x4D;                                  // cmd byte 2
+            dat[4] = 0x53;                                  // cmd byte 3
+            if(value == "0")
+            {// ausschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x30;
+            }
+            if(value == "1")
+            { // einschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x31;
+            }
+            dat[9] = 0x03;                                      // ETX
+
+            int sum = 0;
+            foreach (int number in dat)
+            {
+                sum += number;
+            }
+//            Console.WriteLine(sum);
+            string hilf = sum.ToString("X");
+            string crc1 = hilf.Substring(1, 1);
+            string crc2 = hilf.Substring(2, 1);
+//            Console.WriteLine(hilf);
+
+            byte[] data = new byte[15];
+            byte[] array = Encoding.ASCII.GetBytes(crc1);
+            byte[] array1 = Encoding.ASCII.GetBytes(crc2);
+
+            data[0] = 0x02;
+            data[1] = dat[0];
+            data[2] = dat[1];
+            data[3] = dat[2];
+            data[4] = dat[3];
+            data[5] = dat[4];
+            data[6] = dat[5];
+
+            data[7] = dat[7];
+            data[8] = dat[8];
+            data[9] = dat[9];
+            data[10] = dat[10];
+
+            data[11] = array[0];
+            data[12] = array1[0];
+            data[13] = 0x0D;                                      // Carriage Return
+            data[14] = 0x0A;                                     // Line Feed
+
+
+            string tes = ByteArrayToString(data);
+            return tes;
+        }
+        //-------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------
+        public string RequestADS(string address, string modulenumber, string value)
+        {
+            byte[] dat = new byte[9];
+            byte[] adr = Encoding.ASCII.GetBytes(address);
+            byte[] mod = Encoding.ASCII.GetBytes(modulenumber);
+
+            dat[0] = adr[0];                                // address
+            dat[2] = 0x41;                                  // cmd byte 1
+            dat[3] = 0x44;                                  // cmd byte 2
+            dat[4] = 0x53;                                  // cmd byte 3
+            if (value == "0")
+            {// ausschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x30;
+            }
+            if (value == "1")
+            { // einschalten modul 1
+                dat[5] = 0x30;
+                dat[6] = 0x30;
+                dat[7] = 0x30;
+                dat[8] = 0x31;
+            }
+            dat[9] = 0x03;                                      // ETX
+
+            int sum = 0;
+            foreach (int number in dat)
+            {
+                sum += number;
+            }
+            //            Console.WriteLine(sum);
+            string hilf = sum.ToString("X");
+            string crc1 = hilf.Substring(1, 1);
+            string crc2 = hilf.Substring(2, 1);
+            //            Console.WriteLine(hilf);
+
+            byte[] data = new byte[15];
+            byte[] array = Encoding.ASCII.GetBytes(crc1);
+            byte[] array1 = Encoding.ASCII.GetBytes(crc2);
+
+            data[0] = 0x02;
+            data[1] = dat[0];
+            data[2] = dat[1];
+            data[3] = dat[2];
+            data[4] = dat[3];
+            data[5] = dat[4];
+            data[6] = dat[5];
+
+            data[7] = dat[7];
+            data[8] = dat[8];
+            data[9] = dat[9];
+            data[10] = dat[10];
+
+            data[11] = array[0];
+            data[12] = array1[0];
+            data[13] = 0x0D;                                      // Carriage Return
+            data[14] = 0x0A;                                     // Line Feed
+
+
+            string tes = ByteArrayToString(data);
+            return tes;
+        }
+        //-------------------------------------------------------------------------------------
+        //-------------------------------------------------------------------------------------
         public string RequestData(string address, string modulenumber, string command)
         {
             string f = command;
@@ -78,8 +437,7 @@ namespace HOYA
             return tes;
         }
         //-------------------------------------------------------------------------------------
-
-        //---tools-----------------------------------------------------------------------------
+       //---tools-----------------------------------------------------------------------------
         public static byte[] StringToByteArray(String hex)
         {
             int NumberChars = hex.Length;
@@ -97,4 +455,4 @@ namespace HOYA
         }
 	}
 }
-//IF Command = 'ECS' OR Command = 'LVS' OR Command = 'DRS' OR Command = 'LSS' OR Command = 'BMS' OR Command = 'ADS' OR Command = 'UVE' THEN  
+  
